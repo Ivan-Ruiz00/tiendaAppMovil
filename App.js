562fe2e3b6dca218826ade655a56b6accb7fc4d2
Ivan Ruiz <ivanruiz@MacBook-Pro-de-Ivan.local>
2026-09-04 19:26:33 -0500

    Se agrega el producto.js

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { View, Text, StyleSheet } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titulo}>Mi tienda virtual</Text>
+      <Text style={styles.subtitulo}>
+        Bienvenido a nuestra tienda
+      </Text>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
+    ,
+    alignItems: 'center'
+    ,
+    padding: 20,
   },
 });
